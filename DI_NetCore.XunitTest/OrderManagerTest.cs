5b12cae4fac8000ae243e5f684a58d6201d59d60
Sagar Gupta <aignepal@outlook.com>
2021-03-05 19:35:49 +0545

    DI mock test implemented

--- DI_NetCore.XunitTest/OrderManagerTest.cs
+++ DI_NetCore.XunitTest/OrderManagerTest.cs
@@ -1,4 +1,6 @@
+using DI_NetCore.With_DependencyInjection;
 using DI_NetCore.Without_DependencyInjection;
+using Moq;
 using System;
 using Xunit;
 
@@ -14,5 +16,26 @@ namespace DI_NetCore.XunitTest
 
             Assert.ThrowsAny<Exception>(() => orderManager.Submit(ProductEnum.KeyBoard, "1234567891234567", "1021") );
         }
+
+
+        [Fact]
+        public void Test_OrderManager_DI_OutOfStock_ErrorMessage()
+        {
+            var productStockRepository_Mock = new Mock<IProductStockRepository_WithDI>();
+            productStockRepository_Mock
+                .Setup(m =>  m.IsInStock(It.IsAny<ProductEnum>()))
+                .Returns(false);
+
+            var paymentProcessorMock = new Mock<IPaymentProcessor_WithDI>();
+            var shippingProcessorMock = new Mock<IShippingProcessor_WithDI>();
+
+
+            var orderManager = new OrderManager_WithDI(productStockRepository_Mock.Object, paymentProcessorMock.Object, shippingProcessorMock.Object);
+
+
+            Assert.ThrowsAny<Exception>(() => orderManager.Submit(ProductEnum.KeyBoard, "1234567891234567", "1021"));
+        }
+
+
     }
 }
